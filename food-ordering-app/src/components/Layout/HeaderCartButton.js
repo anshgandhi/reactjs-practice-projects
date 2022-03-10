c288cdf4968fa6d97cbf064446cc49d2e523abb0
Ansh Gandhi <anshgandhi16@gmail.com>
2022-03-09 21:05:16 -0500

    animating cart button

--- food-ordering-app/src/components/Layout/HeaderCartButton.js
+++ food-ordering-app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "./../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((total, item) => {
     return total + item.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    // good practice to cleanup timeouts used in useEffect.
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [cartCtx.items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon></CartIcon>
       </span>
